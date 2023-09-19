f0c24901a39142f1761cfbb3bd4aed7503abb456
eshan <eshan.jain@finverv.com.com>
2023-09-20 00:56:23 +0530

    changed gitignore

---
+++ db_connection.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from mongoengine import connect
+
+from settings.config import DATABASE_NAME
+from settings.config import DATABASE_PASSWORD
+from settings.config import DATABASE_USER
+
+
+class MongoConnect:
+
+    @staticmethod
+    def connect_to_db():
+        uri = f'mongodb+srv://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_NAME}.m29fy3g.mongodb.net/?retryWrites=true&w=majority&ssl=true'
+        connect(db=DATABASE_NAME, username=DATABASE_USER, password=DATABASE_PASSWORD, host=uri)
+        print('DB is connected successfully.')
